ebab5a2b29289f1b2842cfcdb8e757e7c7d7137f
Yoel Monzon <yoelfme@hotmail.com>
2018-05-01 19:16:38 -0500

    [add] migration used to add the field user_id to the table 'topics'

--- priv/repo/migrations/20180417221948_add_users.exs
+++ priv/repo/migrations/20180417221948_add_users.exs
@@ -1,11 +1,15 @@
 defmodule Discuss.Repo.Migrations.AddUsers do
   use Ecto.Migration
 
+  alias Discuss.User
+
   def change do
     create table(:users) do
       add :email, :string
       add :provider, :string
       add :token, :string
+
+      belongs_to(:user, User)
 
       timestamps()
     end
